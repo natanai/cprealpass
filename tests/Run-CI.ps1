b2b6903261adae931e22c621e8f5eaa2b289c234
natanai <109430106+natanai@users.noreply.github.com>
2026-09-14 23:40:54 -0500

    Run E3 owned-presentation contract in cloud CI

--- tests/Run-CI.ps1
+++ tests/Run-CI.ps1
@@ -21,6 +21,7 @@ $tests = @(
     'Test-ConditionArchitecture.ps1',
     'Test-PainArchitecture.ps1',
     'Test-OwnedNameplates.ps1',
+    'Test-E3OwnedPresentation.ps1',
     'Test-DistributionContract.ps1',
     'Test-InstallContract.ps1',
     'Test-FeatureInventory.ps1',
